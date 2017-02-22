69ab21d85fb6738dd95764f81b2ae910f83fc8dd
koldovsky <koldovsky@yahoo.com>
2017-02-22 16:01:22 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// alert('Hello World!');
+// var greetings = 'Hello World!';
+// document.write(greetings);
+// console.log(greetings);
+
+// var userName = prompt('Your name');
+// var greet = 'Hello ' + userName;
+// document.write(greet);
+
+// var age = Number(prompt('Enter your age'));
+// var nextAge = age + 1;
+// var msg = '<br>Soon you will be ' + nextAge;
+// document.write(msg);
+
+// function sum(a, b, c) {
+//     var result = a + b + c;
+//     return result;
+// }
+
+
+// var calcSum = sum(10, 20, 30);
+// document.write(calcSum);
+
+// function checkAge(age) {
+//     if (age > 18) {
+//         return 'Adult';
+//     } else {
+//         return 'Kid';
+//     }
+// }
+
+// var check = checkAge(2);
+// document.write(check);
+
+var squareRoot = Math.sqrt(16);
+document.write(squareRoot);
+
